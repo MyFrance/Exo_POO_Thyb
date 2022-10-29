096431c3f78e0e5c25a1f01e4d8a89b7b7c64fca
Levasseur My France <myfrance.levasseur@gmail.com>
2022-10-29 16:37:48 +0200

    Trouver l'erreur 2

---
+++ trouver_erreur2.py
@@ -0,0 +1,16 @@
+class Cube: 
+    def __init__(self, x=0, y=0, z=0):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def set_position(self, x, y, z): 
+        self.x = x
+        self.y = y
+        self.z = z
+
+cube = Cube()
+cube.set_position(1,2,3)
+print(cube.x)
+print(cube.y)
+print(cube.z)
